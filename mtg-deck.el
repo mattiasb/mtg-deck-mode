271661ddbadebf56fd1976848d538ef273affdb9
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2024-11-15 17:02:05 +0100

    Card: Use `visual-line-fill-column-mode`

--- mtg-deck.el
+++ mtg-deck.el
@@ -44,7 +44,7 @@
   :type 'hook
   :group 'mtg-deck-mode)
 
-(defcustom mtg-deck-card-mode-hook '(view-mode visual-line-mode)
+(defcustom mtg-deck-card-mode-hook '(view-mode visual-line-fill-column-mode)
   "Hook called by `mtg-deck-mode'."
   :type 'hook
   :group 'mtg-deck-mode)
