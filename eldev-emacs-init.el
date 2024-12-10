629f0b6fcb5419f7f0a2087878f8507cc70125f0
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2024-12-10 19:52:05 +0100

    WIP: Init-file

--- eldev-emacs-init.el
+++ eldev-emacs-init.el
@@ -16,51 +16,62 @@
 
 ;;; Code:
 
-(tool-bar-mode -1)
-(menu-bar-mode -1)
+(tool-bar-mode   -1)
+(menu-bar-mode   -1)
 (scroll-bar-mode -1)
 
 (load-theme 'wombat)
 
-(fido-vertical-mode)
-(windmove-default-keybindings)
+(use-package emacs
+  :demand t
+  :hook (emacs-startup   . fido-vertical-mode)
+  :hook (emacs-startup   . windmove-default-keybindings)
 
-(declare-function global-corfu-mode "corfu")
-(use-package corfu :config (global-corfu-mode))
-(use-package visual-fill-column)
+  :custom
+  (inhibit-startup-buffer-menu         t)
+  (inhibit-startup-echo-area-message   "mattiasb")
+  (inhibit-startup-screen              t)
+  (initial-major-mode                  'mtg-deck-mode)
+  (tab-always-indent                   'complete)
+  (text-quoting-style                  'grave)
 
-(setq inhibit-startup-buffer-menu       t
-      inhibit-startup-echo-area-message "mattiasb"
-      inhibit-startup-screen            t
-      initial-major-mode                'mtg-deck-mode
-      tab-always-indent                 'complete
-      text-quoting-style                'grave)
+  :config
+  (let ((deck '("4 Animate Dead"
+                "4 Cryptic Command"
+                "4 Jace, Vryn's Prodigy // Jace, Telepath Unbound"
+                "4 Karakas"
+                "4 Karn, the Great Creator"
+                "4 Nezumi Graverobber // Nighteyes the Desecrator")))
+    (setopt initial-scratch-message (string-join deck "\n"))))
 
-(setq initial-scratch-message
-      (string-join '("4 Animate Dead"
-                     "4 Cryptic Command"
-                     "4 Jace, Vryn's Prodigy // Jace, Telepath Unbound"
-                     "4 Karakas"
-                     "4 Karn, the Great Creator"
-                     "4 Nezumi Graverobber // Nighteyes the Desecrator")
-                   "\n"))
+(use-package corfu
+  :hook (after-init      . global-corfu-mode))
 
-(with-eval-after-load 'mtg-deck
-  (declare-function mtg-deck-mode                 "mtg-deck")
-  (declare-function mtg-deck-show-card            "mtg-deck")
-  (declare-function mtg-deck-update-card-database "mtg-deck")
-  (declare-function mtg-deck-show-card-at-point   "mtg-deck")
-  (declare-function visual-fill-column-mode       "visual-fill-column")
-  (defvar mtg-deck-mode-map)
+(use-package mtg-deck
+  :demand t
+  :hook (mtg-card-mode   . visual-line-fill-column-mode)
+  :hook (mtg-deck-mode   . (lambda ()
+                             (mtg-card-database-update)
+                             (mtg-card-show "Animate Dead")))
+  :bind ( :map mtg-deck-mode-map
+          ( "C-<return>" . mtg-deck-show-card-at-point)
+          ( "<tab>"      . completion-at-point)))
 
-  (define-key mtg-deck-mode-map (kbd "C-<return>")
-              #'mtg-deck-show-card-at-point)
-  (define-key mtg-deck-mode-map (kbd "<tab>")
-              #'completion-at-point)
-  (add-hook 'mtg-deck-card-mode-hook #'visual-fill-column-mode)
-  (mtg-deck-mode)
-  (mtg-card-database-update)
-  (mtg-card-show "Animate Dead"))
+;; (with-eval-after-load 'mtg-deck
+;;   ;; (declare-function visual-fill-column-mode   "visual-fill-column")
+;;   ;; (declare-function mtg-card-database-update  "mtg-card")
+;;   ;; (declare-function mtg-card-show             "mtg-card")
+;;   ;; (defvar mtg-deck-mode-map)
+
+;;   ;; (define-key mtg-deck-mode-map (kbd "C-<return>")
+;;   ;;             #'mtg-deck-show-card-at-point)
+;;   ;; (define-key mtg-deck-mode-map (kbd "<tab>")
+;;   ;;             #'completion-at-point)
+;;   (add-hook 'mtg-deck-card-mode-hook #'visual-fill-column-mode)
+;;   (mtg-card-database-update)
+;;   (mtg-card-show "Animate Dead"))
+
+;; (normal-mode)
 
 (provide 'eldev-emacs-init)
 ;;; eldev-emacs-init.el ends here
